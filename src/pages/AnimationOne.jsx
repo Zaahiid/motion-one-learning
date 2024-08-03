18bfa69d3d656cf5dc13624e99d57ad7f2819f4f
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-04 02:00:03 +0530

    Added animation one 1️⃣

--- src/pages/AnimationOne.jsx
+++ src/pages/AnimationOne.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useRef } from 'react';
+import { animate } from "motion"
 
 const AnimationOne = () => {
+    const boxRef1 = useRef(null);
+
+    useEffect(() => {
+        animate(boxRef1.current, { rotate: 360 }, {
+          repeat: Infinity,
+          duration: 5,
+          ease: "linear",
+          direction: "alternate",
+          repeatDelay: 0.5
+        })
+    },[])
   return (
-    <div className="w-[100vw] h-screen p-10 bg-zinc-900 text-white/70">AnimationOne</div>
+    <div className="w-[100vw] h-screen p-10 bg-zinc-900 text-white/70">
+    <div ref={boxRef1} className="w-32 h-32 bg-pink-800 rounded-md"/>
+    </div>
   )
 }
 
