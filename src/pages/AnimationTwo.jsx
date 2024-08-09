2c144c414af8c5ea994e9c2b59a57b90f31755d6
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-09 23:36:36 +0530

    inView animation 👁

--- src/pages/AnimationTwo.jsx
+++ src/pages/AnimationTwo.jsx
@@ -1,8 +1,39 @@
+import { inView, animate } from "motion";
+import { useEffect } from "react";
 
 const AnimationTwo = () => {
-  return (
-    <div className="w-[100vw] h-screen p-10 bg-zinc-900 text-white/70">AnimationTwo</div>
-  )
-}
+  useEffect(() => {
+    // Define the callback function
+    const handleInView = (info) => {
+      // Animate the element when it enters the viewport
+      animate(info.target, { opacity: 1, scale: 1 });
 
-export default AnimationTwo
+      // Return a function to stop the animation when the element leaves the viewport
+      return (leaveInfo) => {
+        // Stop the animation
+        animate(info.target, { opacity: 0, scale: 0.5 });
+      };
+    };
+    const element = document.querySelector("#myElement");
+    const unobserve = inView(element, handleInView);
+
+    // Clean up the observer on component unmount
+    return () => {
+      unobserve();
+    };
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
+    <div className="w-full h-[200vh] overflow-x-hidden p-10 bg-zinc-900 text-white/70">
+      <div
+        className="w-32 h-32 bg-pink-800 rounded-md"
+        id="myElement"
+        style={{ opacity: 0 }}
+      >
+        <h2 className="text-center pt-[50px]">Zah!d</h2>
+      </div>
+    </div>
+  );
+};
+
+export default AnimationTwo;
